7182c6111b4ec33db59a4fa3579b0f13d12fa71b
Angelo <Angelodimauro91@gmail.com>
2022-08-24 23:07:36 +0200

    Calcolo sconto e INNERHTML

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ console.log(Button)
 
 Button.addEventListener('click',
   function () {
+
+
     console.log("Ciao")
     let Chilometri = parseInt(document.getElementById('Chilometri').value)
     console.log(Chilometri)
@@ -11,5 +13,28 @@ Button.addEventListener('click',
     let Fasciaetà = parseInt(document.getElementById('Fasciaetà').value)
     console.log(Fasciaetà)
 
+
+            //  Chilometri costo KM
+    let CostoBase = Chilometri * 0.21
+    console.log (CostoBase)
+
+    
+                //  Fascia età calcolo
+    if (Fasciaetà < 18) {
+      let ScontoMin = CostoBase * 0.8;
+        console.log(ScontoMin)
+        document.getElementById("COSTO").innerHTML=ScontoMin;
+      }
+
+      if (Fasciaetà > 65) {
+        let ScontoMag = CostoBase * 0.6;
+          console.log(ScontoMag)
+          document.getElementById("COSTO").innerHTML=ScontoMag;
+        }
 }
-);
+
+); 
+
+
+
+
